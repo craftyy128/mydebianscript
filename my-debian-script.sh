9b7a7548704321f20d842c7a0a4f86c03e61dcb5
craftyy128 <148287415+craftyy128@users.noreply.github.com>
2023-10-17 18:47:54 -0400

    Add files via upload

---
+++ my-debian-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Hello!!! welcome to this script so im going to install everything for your minimal debian!"
+
+sleep 2
+
+sudo apt update && sudo apt upgrade
+
+echo "Ok so we updated now its time to get kde!"
+
+sleep 1
+
+echo "this is set for vms rn wana change rhat or smthn hmmm???
+
+sleep 3
+
+sudo apt install plasma-desktop firefox xorg xserver-xorg-core xserver-xorg-video-xf86-video-modesetting xinit xterm 
+
